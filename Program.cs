601c90324349ba9516229e3144f08857768000a7
pigeatgarlic <huyhoangdo0205@gmail.com>
2022-08-08 21:38:25 -0700

    remove middleware

--- Program.cs
+++ Program.cs
@@ -23,24 +23,24 @@ app.UseSwaggerUI(options =>
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
 });
 
-app.Use(async (context,next) => {
-    var body = context.Request.Body;
-    var route = context.Request.Path;
+// app.Use(async (context,next) => {
+//     var body = context.Request.Body;
+//     var route = context.Request.Path;
 
-    byte[] data = new byte[100];
-    await body.ReadAsync(data,0,100);
-    var datastr = Encoding.Default.GetString(data);
-    Console.WriteLine($"incoming {route} : {datastr}");
-    try
-    {
-        await next(context);
-    }
-    catch (System.Exception err)
-    {
-        Console.WriteLine($"error : {err.Message}");
-        throw;
-    }
-});
+//     byte[] data = new byte[100];
+//     await body.ReadAsync(data,0,100);
+//     var datastr = Encoding.Default.GetString(data);
+//     Console.WriteLine($"incoming {route} : {datastr}");
+//     try
+//     {
+//         await next(context);
+//     }
+//     catch (System.Exception err)
+//     {
+//         Console.WriteLine($"error : {err.Message}");
+//         throw;
+//     }
+// });
 
 app.UseAuthorization();
 
