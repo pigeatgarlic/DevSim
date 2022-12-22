a85d3d5e273db9989b3bbbb3a6bec236f9566053
pigeatgarlic <huyhoangdo0205@gmail.com>
2022-12-22 11:55:16 +0530

    enable websocket

--- Controllers/SocketController.cs
+++ Controllers/SocketController.cs
@@ -1,105 +1,124 @@
-// using Microsoft.AspNetCore.Mvc;
-// using DevSim.Interfaces;
-// using DevSim.Enums;
-// using System.Text;
-// using System.Net.WebSockets;
+using Microsoft.AspNetCore.Mvc;
+using DevSim.Interfaces;
+using DevSim.Enums;
+using System.Text;
+using System.Net.WebSockets;
 
-// namespace DevSim.Controllers
-// {
-//     [ApiController]
-//     [Route("Socket")]
-//     public class SocketController : ControllerBase
-//     {
-//         private readonly ILogger<KeyboardController> _log;
-//         private readonly IKeyboardMouseInput _key;
-//         private readonly IGamepadInput _gamepad;
-//         public SocketController(ILogger<KeyboardController> logger,
-//                                 IGamepadInput gamepad,
-//                                 IKeyboardMouseInput key) {
-//             _log = logger;
-//             _key = key;
-//             _gamepad = gamepad;
-//         }
+namespace DevSim.Controllers
+{
+    [ApiController]
+    [Route("Socket")]
+    public class SocketController : ControllerBase
+    {
+        private readonly ILogger<KeyboardController> _log;
+        private readonly IKeyboardMouseInput _key;
+        private readonly IGamepadInput _gamepad;
+        public SocketController(ILogger<KeyboardController> logger,
+                                IGamepadInput gamepad,
+                                IKeyboardMouseInput key) {
+            _log = logger;
+            _key = key;
+            _gamepad = gamepad;
+        }
 
-//         [HttpGet("Handshake")]
-//         public async Task Get(string token)
-//         {
-//             var context = ControllerContext.HttpContext;
-//             if (context.WebSockets.IsWebSocketRequest)
-//             {
-//                 var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-//                 await Handle(webSocket);
-//             }
-//         }
+        [HttpGet("Handshake")]
+        public async Task Get(string? token)
+        {
+            var context = ControllerContext.HttpContext;
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                await Handle(webSocket);
+            }
+        }
 
-//         public async Task Handle(WebSocket ws)
-//         {
-//             try
-//             {
-//                 do
-//                 {
-//                     using (var memoryStream = new MemoryStream())
-//                     {
-//                         var message = ReceiveMessage(ws, memoryStream).Result;
-//                         if (message.Count > 0) {
-//                             var receivedMessage = Encoding.UTF8.GetString(memoryStream.ToArray());
+        private async Task Handle(WebSocket ws)
+        {
+            try
+            {
+                do
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        var message = ReceiveMessage(ws, memoryStream).Result;
+                        if (message.Count > 0) {
+                            var receivedMessage = Encoding.UTF8.GetString(memoryStream.ToArray());
 
-//                             var arr = receivedMessage.Split("|");
-//                             switch (arr[0])
-//                             {
-//                                 case "mm":
-//                                 case "mw":
-//                                 case "mu":
-//                                 case "md":
+                            var arr = receivedMessage.Split("|");
+                            switch (arr[0])
+                            {
+                                case "mmr":
+                                    await _key.ToggleRelativeMouse(true);
+                                    _key.SendMouseMove(Single.Parse(arr[1]),Single.Parse(arr[2]));
+                                    break;
+                                case "mma":
+                                    await _key.ToggleRelativeMouse(false);
+                                    _key.SendMouseMove(Single.Parse(arr[1]),Single.Parse(arr[2]));
+                                    break;
+                                case "mw":
+                                    _key.SendMouseWheel(Int32.Parse(arr[1]));
+                                    break;
+                                case "mu":
+                                    _key.SendMouseButtonAction((ButtonCode)Int32.Parse(arr[1]),ButtonAction.Up);
+                                    break;
+                                case "md":
+                                    _key.SendMouseButtonAction((ButtonCode)Int32.Parse(arr[1]),ButtonAction.Up);
+                                    break;
 
-//                                 case "kd":
-//                                 case "ku":
+                                case "kd":
+                                    _key.SendKeyDown(arr[1]);
+                                    break;
+                                case "ku":
+                                    _key.SendKeyUp(arr[1]);
+                                    break;
+                                case "kr":
+                                    _key.SetKeyStatesUp();
+                                    break;
 
-//                                 case "gs":
-//                                     _gamepad.pressAxis(Int32.Parse(arr[1]),Single.Parse(arr[2]));
-//                                     break;
-//                                 case "ga":
-//                                     _gamepad.pressAxis(Int32.Parse(arr[1]),Single.Parse(arr[2]));
-//                                     break;
-//                                 case "gb":
-//                                     _gamepad.pressButton(Int32.Parse(arr[1]),arr[2] == "1");
-//                                     break;
-//                                 default:
-//                                 break;
-//                             }
-//                         }
-//                     }
-//                 } while (ws.State == WebSocketState.Open);
-//             }
-//             catch (Exception ex) { }
-//             _log.LogInformation("Connection closed");
-//         }
+                                case "gs":
+                                    _gamepad.pressSlider(Int32.Parse(arr[1]),Single.Parse(arr[2]));
+                                    break;
+                                case "ga":
+                                    _gamepad.pressAxis(Int32.Parse(arr[1]),Single.Parse(arr[2]));
+                                    break;
+                                case "gb":
+                                    _gamepad.pressButton(Int32.Parse(arr[1]),arr[2] == "1");
+                                    break;
 
-//         private async Task<WebSocketReceiveResult> ReceiveMessage(WebSocket ws, Stream memoryStream)
-//         {
-//             var readBuffer = new ArraySegment<byte>(new byte[4 * 1024]);
-//             WebSocketReceiveResult result;
+                                default:
+                                break;
+                            }
+                        }
+                    }
+                } while (ws.State == WebSocketState.Open);
+            }
+            catch (Exception ex) { }
+            _log.LogInformation("Connection closed");
+        }
 
-//             do
-//             {
-//                 result = await ws.ReceiveAsync(readBuffer, CancellationToken.None);
-//                 await memoryStream.WriteAsync(readBuffer.Array, readBuffer.Offset, result.Count,
-//                     CancellationToken.None);
-//             } while (!result.EndOfMessage);
-//             return result;
-//         }
+        private async Task<WebSocketReceiveResult> ReceiveMessage(WebSocket ws, Stream memoryStream)
+        {
+            var readBuffer = new ArraySegment<byte>(new byte[4 * 1024]);
+            WebSocketReceiveResult result;
 
-//         public async Task SendMessage(WebSocket ws, string msg)
-//         {
-//             var bytes = Encoding.UTF8.GetBytes(msg);
-//             var buffer = new ArraySegment<byte>(bytes);
+            do
+            {
+                result = await ws.ReceiveAsync(readBuffer, CancellationToken.None);
+                await memoryStream.WriteAsync(readBuffer.Array, readBuffer.Offset, result.Count,
+                    CancellationToken.None);
+            } while (!result.EndOfMessage);
+            return result;
+        }
 
-//             try
-//             {
-//                 await ws.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
-//             } catch { _log.LogError("Fail to send websocket to client"); }
-//         }
+        private async Task SendMessage(WebSocket ws, string msg)
+        {
+            var bytes = Encoding.UTF8.GetBytes(msg);
+            var buffer = new ArraySegment<byte>(bytes);
 
-
-//     }
-// }
+            try
+            {
+                await ws.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+            } catch { _log.LogError("Fail to send websocket to client"); }
+        }
+    }
+}
